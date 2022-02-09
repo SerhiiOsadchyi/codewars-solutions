5a8bcdcdef1a34807b875fe557b527726e06b810
SergiiOsadchyi <serge-o@ukr.net>
2022-02-09 17:45:12 +0200

    first commit

---
+++ orderTasks.js
@@ -0,0 +1,42 @@
+/*
+Descending Order
+Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. 
+Essentially, rearrange the digits to create the highest possible number.
+
+Examples:
+Input: 42145 Output: 54421
+
+Input: 145263 Output: 654321
+
+Input: 123456789 Output: 987654321
+*/
+
+descendingOrder(1)
+
+function descendingOrder(n){
+    const strArray = [...n.toString()]
+    let index = 0
+    let outputStr = ''
+
+    while (strArray.length > 0) {
+        index = getMinNumber(strArray)
+        outputStr += strArray[index]
+        strArray.splice(index, 1)
+    }
+
+    return +outputStr
+}
+
+function getMinNumber(numStringArray) {
+    let max = +numStringArray[0]
+    let index = 0
+
+    for (let i = 1; i < numStringArray.length; i++) {
+        if (max < +numStringArray[i]) {
+            max = +numStringArray[i]
+            index = i
+        }
+    }
+
+    return index
+}
